b43b3450f594e3c2340da1f809cda796e54f3eff
jeinieldjs <silvestrejeiniel@gmail.com>
2023-11-28 13:34:28 +0800

    added controller test, controller, and erb for tasks

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,62 +1,50 @@
 class CategoriesController < ApplicationController
-    before_action :set_category, only: %i[ show edit update destroy ]
-  
+    before_action :set_category, only: [:show, :edit, :update, :destroy]
+    
     def index
-      @categories = Category.all
+        @categories = Category.all
     end
-  
+
     def show
     end
-  
+
     def new
-      @category = Category.new
+        @category = Category.new
     end
-  
+
+    def create
+        @category = Category.new(category_params)
+
+        if @category.save
+            redirect_to category_path(@category), notice: 'Successfully created new category'
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
     def edit
     end
-  
-    def create
-      @category = Category.new(category_params)
-  
-      respond_to do |format|
-        if @category.save
-          format.html { redirect_to category_url(@category), notice: "Category was successfully created." }
-          format.json { render :show, status: :created, location: @category }
-        else
-          format.html { render :new, status: :unprocessable_entity }
-          format.json { render json: @category.errors, status: :unprocessable_entity }
-        end
-      end
-    end
-  
+
     def update
-      respond_to do |format|
         if @category.update(category_params)
-          format.html { redirect_to category_url(@category), notice: "Category was successfully updated." }
-          format.json { render :show, status: :ok, location: @category }
+            redirect_to category_path(@category), notice: 'Successfully updated.'
         else
-          format.html { render :edit, status: :unprocessable_entity }
-          format.json { render json: @category.errors, status: :unprocessable_entity }
+            render :edit, status: :unprocessable_entity
         end
-      end
     end
-  
+
     def destroy
-      @category.destroy!
-  
-      respond_to do |format|
-        format.html { redirect_to categories_url, notice: "Category was successfully destroyed." }
-        format.json { head :no_content }
-      end
+        @category.destroy
+        redirect_to categories_path, notice: 'Successfully deleted the category.'
     end
-  
+
     private
-  
+
     def set_category
-      @category = Category.find(params[:id])
+        @category = Category.find(params[:id])
     end
-  
+
     def category_params
-      params.require(:category).permit(:title, :description)
+        params.require(:category).permit(:title, :description)
     end
-  end
+end
---
+++ test/controllers/tasks_controller_test.rb
@@ -0,0 +1,53 @@
+require "test_helper"
+
+class TasksControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  setup do
+    @category = categories(:one)
+    @task = tasks(:one)
+  end
+
+  test "should get index" do
+    get category_tasks_path(@category)
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_category_task_path(@category)
+    assert_response :success
+  end
+
+  test "should create task" do
+    assert_difference("Task.count") do
+      post category_tasks_path(@category), params: {task: {title: @task.title,  description: @task.description}}
+    end
+
+    assert_redirected_to category_task_path(@category, Task.last)
+  end
+
+  test "should show task" do
+    get category_task_path(@category, @task)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_category_task_path(@category, @task)
+    assert_response :success
+  end
+
+  test "should update task" do
+    patch category_task_path(@category, @task), params: {task: {title: @task.title, description: @task.description}}
+    assert_redirected_to category_task_path(@category, @task)
+  end
+
+  test "should destroy task" do
+    assert_difference("Task.count", -1) do
+      delete category_task_path(@category, @task)
+    end
+
+    assert_redirected_to category_tasks_path(@category)
+  end
+end
+
