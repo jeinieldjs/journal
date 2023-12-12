6c3619531991ed38fd45842893062e9db8b3a8d9
jeinieldjs <silvestrejeiniel@gmail.com>
2023-12-12 15:41:52 +0800

    added test for user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,17 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test 'should have many categories' do 
+    user = users(:one)
+    assert_respond_to user, :categories
+  end
+
+  test 'should destroy associated categories' do 
+    user = users(:one)
+    category = user.categories.create(title: 'Sample')
+
+    assert_difference('Category.count', -1) do
+      user.destroy
+    end
+  end
 end
